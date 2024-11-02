0754fea8cb873aaf8dfff7a4570aaa7d271128a2
OliwierWilbrandt <161207691+OliwierWilbrandt@users.noreply.github.com>
2024-11-02 16:24:09 +0100

    dodanie zestaw 3

--- src/Zestaw_3.java
+++ src/Zestaw_3.java
@@ -1,7 +1,59 @@
 public class Zestaw_3 {
-    public static void main(String[] args) {
 
+    //Zad14
 
+    public static int dwumian(int n, int k) {
+        int wynik = 1;
 
+        for (int i = 0; i < k; i++) {
+            wynik = wynik * (n - i) / (i + 1);
+
+        }
+        return wynik;
     }
+
+    public static void trojkatpascala(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k <= i; k++) {
+                System.out.print(dwumian(i, k) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Zadanie 14\n");
+        int n = 11;
+        trojkatpascala(n);
+
+        System.out.print("Zadanie 15\n");
+        int liczba = 15;
+
+        if (czypalindrom(liczba)) {
+            System.out.println("Liczba " + liczba + " jest palindromem");
+        } else {
+            System.out.println("Liczba " + liczba + " nie jest palindromem");
+        }
+    }
+
+
+    //15
+
+    public static boolean czypalindrom(int n) {
+        int poczatkowaliczba = n;
+        int odwroconaliczba = 0;
+
+        while (n != 0) {
+            int ostatniacyfra = n % 10;
+            odwroconaliczba = odwroconaliczba * 10 + ostatniacyfra;
+            n = n / 10;
+        }
+        return poczatkowaliczba == odwroconaliczba;
+    }
+
 }
